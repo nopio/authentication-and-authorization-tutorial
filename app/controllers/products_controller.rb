c1f19e2d5132c4183f679b76e77335b93b30e60c
Damian <damian.mamla@gmail.com>
2016-09-18 18:51:28 +0200

    Step 16 - Restrict access to products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @products = Product.all
   end
