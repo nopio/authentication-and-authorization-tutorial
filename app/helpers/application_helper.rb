cbc95014fd61dafe5aa4bdda35ed3d5b7b17912d
Damian <damian.mamla@gmail.com>
2016-09-18 17:51:35 +0200

    Step 4 - Add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def boostrap_class(alert)
+    { success: 'alert-success', error: 'alert-danger', notice: 'alert-success', warning: 'alert-warning',
+      danger: 'alert-danger', alert: 'alert-danger' }[alert.to_sym]
+  end
+
+  def flash_messages(_opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{boostrap_class(msg_type.to_sym)} fade in") do
+        concat(content_tag(:button, id: "close-button", class: "close", type: :button, data: { dismiss: 'alert' }, "aria-label" => :Close) do
+          concat content_tag(:span, "&times;".html_safe, "aria-hidden" => true)
+        end)
+        concat message
+      end)
+    end
+    nil
+  end
 end
