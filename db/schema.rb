bd8e101145ed7fdce9e6b94a62ef606f11b8c220
Damian <damian.mamla@gmail.com>
2016-09-18 18:55:29 +0200

    Step 17 -  Add new field to user.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160918163008) do
+ActiveRecord::Schema.define(version: 20160918165229) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160918163008) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
