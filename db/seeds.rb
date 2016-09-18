6fecf408416041818f0da8a477c808a80191a213
Damian <damian.mamla@gmail.com>
2016-09-18 19:03:10 +0200

    Step 20 - Populating the Database with seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Role.create(name: :admin)
+Role.create(name: :client)
+user1 = User.create(username: 'Nicole',
+								    email: 'admin@gmail.com',
+								    password: 'password1234',
+								    password_confirmation: 'password1234')
+user1.add_role(:admin)
+user2 = User.create(username: 'Bruce',
+								    email: 'client@gmail.com',
+								    password: 'password1234',
+								    password_confirmation: 'password1234')
+user2.add_role(:client)
+1.upto(5) do |i|
+	Product.create(name: "monitor #{i}",
+								 price: 600 * i,
+								 user: user1)
+end
+1.upto(5) do |i|
+	Product.create(name: "keyboard #{i}",
+								 price: 200 * i,
+								 user: user2)
+end
